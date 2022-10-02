556042d5e69e0fd8bc3094a7e5c7da333dc361be
Wilmer Molina <wilmeramr@gmail.com>
2022-10-02 18:45:30 -0300

    logo unica

--- LambtechRele/Form1.Designer.cs
+++ LambtechRele/Form1.Designer.cs
@@ -46,9 +46,10 @@ namespace LambtechRele
             // btnDesconectar
             // 
             this.btnDesconectar.Highlight = true;
-            this.btnDesconectar.Location = new System.Drawing.Point(376, 30);
+            this.btnDesconectar.Location = new System.Drawing.Point(282, 24);
+            this.btnDesconectar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnDesconectar.Name = "btnDesconectar";
-            this.btnDesconectar.Size = new System.Drawing.Size(116, 40);
+            this.btnDesconectar.Size = new System.Drawing.Size(87, 32);
             this.btnDesconectar.Style = MetroFramework.MetroColorStyle.Red;
             this.btnDesconectar.TabIndex = 8;
             this.btnDesconectar.Text = "Desconectar";
@@ -57,9 +58,10 @@ namespace LambtechRele
             // btnConectar
             // 
             this.btnConectar.Highlight = true;
-            this.btnConectar.Location = new System.Drawing.Point(258, 30);
+            this.btnConectar.Location = new System.Drawing.Point(194, 24);
+            this.btnConectar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConectar.Name = "btnConectar";
-            this.btnConectar.Size = new System.Drawing.Size(112, 39);
+            this.btnConectar.Size = new System.Drawing.Size(84, 32);
             this.btnConectar.Style = MetroFramework.MetroColorStyle.Black;
             this.btnConectar.TabIndex = 7;
             this.btnConectar.Text = "Conectar";
@@ -68,10 +70,11 @@ namespace LambtechRele
             // cmbPorts
             // 
             this.cmbPorts.FormattingEnabled = true;
-            this.cmbPorts.ItemHeight = 24;
-            this.cmbPorts.Location = new System.Drawing.Point(106, 33);
+            this.cmbPorts.ItemHeight = 23;
+            this.cmbPorts.Location = new System.Drawing.Point(80, 27);
+            this.cmbPorts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cmbPorts.Name = "cmbPorts";
-            this.cmbPorts.Size = new System.Drawing.Size(137, 30);
+            this.cmbPorts.Size = new System.Drawing.Size(104, 29);
             this.cmbPorts.TabIndex = 6;
             // 
             // metroPanel1
@@ -83,21 +86,23 @@ namespace LambtechRele
             this.metroPanel1.Controls.Add(this.btnCircuito1);
             this.metroPanel1.HorizontalScrollbarBarColor = true;
             this.metroPanel1.HorizontalScrollbarHighlightOnWheel = false;
-            this.metroPanel1.HorizontalScrollbarSize = 10;
-            this.metroPanel1.Location = new System.Drawing.Point(65, 105);
+            this.metroPanel1.HorizontalScrollbarSize = 8;
+            this.metroPanel1.Location = new System.Drawing.Point(49, 85);
+            this.metroPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.metroPanel1.Name = "metroPanel1";
-            this.metroPanel1.Size = new System.Drawing.Size(494, 100);
+            this.metroPanel1.Size = new System.Drawing.Size(370, 81);
             this.metroPanel1.TabIndex = 5;
             this.metroPanel1.VerticalScrollbarBarColor = true;
             this.metroPanel1.VerticalScrollbarHighlightOnWheel = false;
-            this.metroPanel1.VerticalScrollbarSize = 10;
+            this.metroPanel1.VerticalScrollbarSize = 8;
             // 
             // btnCircuito4
             // 
             this.btnCircuito4.Highlight = true;
-            this.btnCircuito4.Location = new System.Drawing.Point(379, 49);
+            this.btnCircuito4.Location = new System.Drawing.Point(284, 40);
+            this.btnCircuito4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCircuito4.Name = "btnCircuito4";
-            this.btnCircuito4.Size = new System.Drawing.Size(81, 34);
+            this.btnCircuito4.Size = new System.Drawing.Size(61, 28);
             this.btnCircuito4.Style = MetroFramework.MetroColorStyle.Black;
             this.btnCircuito4.TabIndex = 10;
             this.btnCircuito4.Text = "4";
@@ -106,9 +111,10 @@ namespace LambtechRele
             // btnCircuito3
             // 
             this.btnCircuito3.Highlight = true;
-            this.btnCircuito3.Location = new System.Drawing.Point(262, 49);
+            this.btnCircuito3.Location = new System.Drawing.Point(196, 40);
+            this.btnCircuito3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCircuito3.Name = "btnCircuito3";
-            this.btnCircuito3.Size = new System.Drawing.Size(81, 34);
+            this.btnCircuito3.Size = new System.Drawing.Size(61, 28);
             this.btnCircuito3.Style = MetroFramework.MetroColorStyle.Black;
             this.btnCircuito3.TabIndex = 9;
             this.btnCircuito3.Text = "3";
@@ -117,9 +123,10 @@ namespace LambtechRele
             // btnCircuito2
             // 
             this.btnCircuito2.Highlight = true;
-            this.btnCircuito2.Location = new System.Drawing.Point(149, 47);
+            this.btnCircuito2.Location = new System.Drawing.Point(112, 38);
+            this.btnCircuito2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCircuito2.Name = "btnCircuito2";
-            this.btnCircuito2.Size = new System.Drawing.Size(81, 36);
+            this.btnCircuito2.Size = new System.Drawing.Size(61, 29);
             this.btnCircuito2.Style = MetroFramework.MetroColorStyle.Black;
             this.btnCircuito2.TabIndex = 8;
             this.btnCircuito2.Text = "2";
@@ -130,18 +137,20 @@ namespace LambtechRele
             this.lblCircuitos.AutoSize = true;
             this.lblCircuitos.FontSize = MetroFramework.MetroLabelSize.Tall;
             this.lblCircuitos.FontWeight = MetroFramework.MetroLabelWeight.Bold;
-            this.lblCircuitos.Location = new System.Drawing.Point(209, 8);
+            this.lblCircuitos.Location = new System.Drawing.Point(157, 6);
+            this.lblCircuitos.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblCircuitos.Name = "lblCircuitos";
-            this.lblCircuitos.Size = new System.Drawing.Size(65, 25);
+            this.lblCircuitos.Size = new System.Drawing.Size(63, 25);
             this.lblCircuitos.TabIndex = 7;
             this.lblCircuitos.Text = "RELES";
             // 
             // btnCircuito1
             // 
             this.btnCircuito1.Highlight = true;
-            this.btnCircuito1.Location = new System.Drawing.Point(33, 47);
+            this.btnCircuito1.Location = new System.Drawing.Point(25, 38);
+            this.btnCircuito1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCircuito1.Name = "btnCircuito1";
-            this.btnCircuito1.Size = new System.Drawing.Size(81, 35);
+            this.btnCircuito1.Size = new System.Drawing.Size(61, 28);
             this.btnCircuito1.Style = MetroFramework.MetroColorStyle.Black;
             this.btnCircuito1.TabIndex = 2;
             this.btnCircuito1.Text = "1";
@@ -149,28 +158,31 @@ namespace LambtechRele
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::LambtechRele.Properties.Resources.lambtech;
-            this.pictureBox1.Location = new System.Drawing.Point(151, 211);
+            this.pictureBox1.Image = global::LambtechRele.Properties.Resources.WhatsApp_Image_2022_10_02_at_12_59_49;
+            this.pictureBox1.Location = new System.Drawing.Point(113, 171);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(286, 89);
+            this.pictureBox1.Size = new System.Drawing.Size(214, 72);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 9;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(592, 301);
+            this.ClientSize = new System.Drawing.Size(444, 245);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnDesconectar);
             this.Controls.Add(this.btnConectar);
             this.Controls.Add(this.cmbPorts);
             this.Controls.Add(this.metroPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(15, 60, 15, 16);
             this.Resizable = false;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
--- LambtechRele/Properties/Resources.Designer.cs
+++ LambtechRele/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace LambtechRele.Properties {
     // a través de una herramienta como ResGen o Visual Studio.
     // Para agregar o quitar un miembro, edite el archivo .ResX y, a continuación, vuelva a ejecutar ResGen
     // con la opción /str o recompile su proyecto de VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -63,9 +63,29 @@ namespace LambtechRele.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap acceso {
+            get {
+                object obj = ResourceManager.GetObject("acceso", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap lambtech {
             get {
                 object obj = ResourceManager.GetObject("lambtech", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap WhatsApp_Image_2022_10_02_at_12_59_49 {
+            get {
+                object obj = ResourceManager.GetObject("WhatsApp Image 2022-10-02 at 12.59.49", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
